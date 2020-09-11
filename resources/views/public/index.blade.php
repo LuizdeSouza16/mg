d9795da12a84e7bccf759b4d0d1493bce8344507
LuizdeSouza16 <luizdesouza16@gmail.com>
2020-09-11 16:30:41 -0300

    imagem responsiva

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,34 +1,32 @@
 @extends('public.app')
 
 @section('content')
-    <div class="col-xs-12 m-3">
+    <div class="col-xs-12 col-md-10 mx-auto">
         <h1 class="text-danger">Últimos Conteúdos</h1>
 
         @foreach ($posts as $post)
-        <hr>
-        <article class="post-h">
-            <div class="row">
-                <div class="col-lg-4 post-h__image">
-                    <a
-                        href="{{ route('noticia', $post->id) }} ">
-                        <img src="{{ url("storage/{$post->post_img}") }} "
-                            alt="{{ $post->title }}"
-                            title="{{ $post->title }}"
-                            style="width: auto; height: 114px" >
-                    </a>
+            <hr>
+            <article class="post-h">
+                <div class="row">
+                    <div class="col-lg-4 ">
+                        <a href="{{ route('noticia', $post->id) }} ">
+                            <div class="col-xs-12 col-lg-10">
+                                <img src="{{ url("storage/{$post->post_img}") }} " alt="{{ $post->title }}"
+                                    title="{{ $post->title }}" class="img-fluid">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-8">
+                        <a href="{{ route('noticia', $post->id) }} ">
+                            <h2 class="text-danger">{{ $post->title }}</h2>
+                            <p class="text-muted"> {{ $post->resume }} </p>
+                            <div class="text-dark">
+                                <span><i class="fa fa-clock-o mr-1"></i> {{ $post->created_at->format('d/m/y H:i') }}</span>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-                <div class="col-lg-8">
-                    <a
-                        href="{{ route('noticia', $post->id) }} ">
-                        <h2 class="text-danger">{{ $post->title }}</h2>
-                        <p class="text-muted"> {{ $post->resume }} </p>
-                        <div class="text-dark">
-                            <span><i class="fa fa-clock-o mr-1"></i> {{ $post->created_at->format('d/m/y H:i') }}</span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </article>
+            </article>
         @endforeach
 
         {!! $posts->links() !!}
--- resources/views/public/show.blade.php
+++ resources/views/public/show.blade.php
@@ -6,9 +6,9 @@
     <div class="container">
         <div class="row">
             <h1 class="text-center mb-2"> {{ $post->title}} </h1>
-            <div class="col-12">
+            <div class="col-xs-12 col-sm-12 vol-md-12">
               <div class="text-center">
-                <img src="{{ url("storage/{$post->post_img}") }}" alt="{{ $post->title }}" >
+                <img src="{{ url("storage/{$post->post_img}") }}" alt="{{ $post->title }}" class="img-fluid">
               </div>
               <p><button class="mx-5 my-1 btn-xs btn-primary"> {{ $post->tag->tag }} </button></p>
               <h6 class="mx-5 my-1 text-muted"> {{ $post->resume }} </h6>
